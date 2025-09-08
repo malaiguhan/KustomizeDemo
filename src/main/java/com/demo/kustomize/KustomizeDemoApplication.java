c1401bf86d041622b05348641ba498b48ae40867
Malai Guhan <ramalaiguhan@gmail.com>
2025-09-08 13:35:17 +0530

    Optimize Imports

--- src/main/java/com/demo/kustomize/KustomizeDemoApplication.java
+++ src/main/java/com/demo/kustomize/KustomizeDemoApplication.java
@@ -1,30 +1,16 @@
 package com.demo.kustomize;
 
 import jakarta.servlet.http.HttpServletRequest;
-import org.apache.spark.broadcast.Broadcast;
-import org.apache.spark.sql.*;
-import org.apache.spark.sql.expressions.Window;
-import org.apache.spark.sql.expressions.WindowSpec;
-import org.apache.spark.sql.types.DataTypes;
-import org.apache.spark.sql.types.StructField;
-import org.apache.spark.sql.types.StructType;
-import org.apache.spark.util.LongAccumulator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.WebUtils;
-import scala.reflect.ClassTag;
 
-import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import static org.apache.spark.sql.functions.*;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 @RestController
